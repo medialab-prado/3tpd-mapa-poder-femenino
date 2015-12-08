4ec8e1e9c86043a254267c959920964cacf52e5a
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2015-12-09 00:55:31 +0100

    Arreglos al home, se agregaron algunas visualizaciones de política

--- js/vis/politica/gobierno.js
+++ js/vis/politica/gobierno.js
@@ -7,9 +7,10 @@ function truncate(str, maxLength, suffix) {
 	return str;
 }
 
-var margin = {top: 20, right: 100, bottom: 0, left: 20},
-	width = 1020,
-	height = 350;
+var container = document.getElementById('gobierno');
+var margin = {top: 30, right: 150, bottom: 0, left: 20},
+	width = container.clientWidth * 0.8,
+	height = 700;
 
 var start_year = 1977,
 	end_year = 2015;
@@ -35,56 +36,78 @@ var svg = d3.select("#gobierno").append("svg")
 	.append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv('../../datasets/gobierno.csv', function(gobierno){
+d3.csv('../datasets/gobierno.csv', function(gobierno){
 
+  var cache = {}
   gobierno.forEach(function(d){
     switch(d.legislatura){
-      case 'Constituyente': d.legislaturaN = 1; break;
-      case 'Primera': d.legislaturaN = 2;break;
-      case 'Segunda': d.legislaturaN = 3;break;
-      case 'Tercera': d.legislaturaN = 4;break;
-      case 'Cuarta': d.legislaturaN = 5;break;
-      case 'Quinta': d.legislaturaN = 6;break;
-      case 'Sexta': d.legislaturaN = 7;break;
-      case 'Septima': d.legislaturaN = 8;break;
-      case 'Octava': d.legislaturaN = 9;break;
-      case 'Novena': d.legislaturaN = 10;break;
-      case 'Decima': d.legislaturaN = 11;break;
+      case 'Constituyente': d.legislaturaN = 10; break;
+      case 'Primera': d.legislaturaN = 20;break;
+      case 'Segunda': d.legislaturaN = 30;break;
+      case 'Tercera': d.legislaturaN = 40;break;
+      case 'Cuarta': d.legislaturaN = 50;break;
+      case 'Quinta': d.legislaturaN = 60;break;
+      case 'Sexta': d.legislaturaN = 70;break;
+      case 'Septima': d.legislaturaN = 80;break;
+      case 'Octava': d.legislaturaN = 90;break;
+      case 'Novena': d.legislaturaN = 100;break;
+      case 'Decima': d.legislaturaN = 110;break;
       default: d.legislaturaN = 0;
     }
+	d.date = d.year; 
+	d_ = new Date(d.year.split('/').reverse().join("/"));
+	d.year = d_.getFullYear();
+	d.month = d_.getMonth() + 1;
+	
+	if(!cache.hasOwnProperty(d.legislatura)){
+		cache[d.legislatura]={};
+		cache[d.legislatura][d.year]={};
+	}
+	if(!cache[d.legislatura].hasOwnProperty(d.year)){
+		cache[d.legislatura][d.year]={};
+	}
+	if(!cache[d.legislatura][d.year].hasOwnProperty(d.month)){
+		cache[d.legislatura][d.year][d.month]=1;
+	}
+	
+	if(Object.keys(cache[d.legislatura]).length >1){
+		d.legislaturaN+=Object.keys(cache[d.legislatura]).length-1;
+	}
+	d.legislatura+=" (" +d.month+"/"+d.year+")";	
+	d.mujer = d.sexo=='Mujer'
   })
 
-  gobierno.forEach(function(d){ d.year = new Date(d.year.split('/').reverse().join("/")).getFullYear() })
-  gobierno.forEach(function(d){ d.mujer = d.sexo=='Mujer' })
-
+  
   var byLegislatura = d3.nest().key(d3.f('legislaturaN')).entries(gobierno);
   var byYear = d3.nest().key(d3.f('year')).entries(gobierno);
 
+  
   byLegislatura.forEach(function (d){
     d.name=d.values[0].legislatura;
     total = 0
     periods = {}
     d.values.forEach(function (a) {
-      if(!periods.hasOwnProperty(a.year)){
-        periods[a.year]=[0,0];
+      var key = a.year+""+a.month;
+      if(!periods.hasOwnProperty(key)){
+        periods[key]=[0,0];
       }
       if(a.mujer){
         total++;
-        periods[a.year][0]++;
+        periods[key][0]++;
       }
-			periods[a.year][1]++;
+	  periods[key][1]++;
     })
     periods_ = [];
     for (p in periods){
-			p_ = +parseFloat((periods[p][0]/periods[p][1])*100).toFixed(0)
-      periods_.push([+p,p_,periods[p][0],periods[p][1]-periods[p][0],d.name])
+    	p_ = +parseFloat((periods[p][0]/periods[p][1])*100).toFixed(0)
+    	periods_.push([+p,p_,periods[p][0],periods[p][1]-periods[p][0],d.name])
     }
-
     d.total = total;
     d.periods = periods_;
   });
-data = byLegislatura;
-x.domain([start_year, end_year]);
+  
+	data = byLegislatura;
+	x.domain([start_year, end_year]);
 	var xScale = d3.scale.linear()
 		.domain([start_year, end_year])
 		.range([0, width])
@@ -112,7 +135,7 @@ x.domain([start_year, end_year]);
 			.range([2, 12]);
 
 		squares
-			.attr("x", function(d, i) { return xScale(d[0]); })
+			.attr("x", function(d, i) { var year = d[0].toString().substr(0,4); return xScale(year); })
 			.attr("y", j*20+20)
 			.attr("width", function(d) { return rScale(d[1]); })
       .attr("height", function(d) { return rScale(d[1]); })
@@ -122,7 +145,7 @@ x.domain([start_year, end_year]);
 
 		text
 			.attr("y", j*20+25)
-			.attr("x",function(d, i) { return xScale(d[0])-5; })
+			.attr("x",function(d, i) { var year = d[0].toString().substr(0,4); return xScale(year)-5; })
 			.attr("class","value")
 			.text(function(d){ return d[1]+"%"; })
 			.style("fill", function(d) {return c2(d[1])})
@@ -150,16 +173,22 @@ x.domain([start_year, end_year]);
 		d3.select(g).selectAll("text.value").style("display","none");
 	}
   function showTooltip(d){
-		template = _.template("<strong><%=nombre%></strong> - <%= cargo %>")
-    year = byYear.filter(function (y){return +y.key==d[0];});
-    year = year.pop();
-    mujeres = year.values.filter(function(m){return m.mujer});
-		text = "<span class='right'>"+d[0]+"</span><strong> Legislatura:</strong> "+d[4]+"<br>";
-		text +="<strong> Mujeres:</strong> "+d[1]+"%<p>";
-		mujeres.forEach(function(m){
+    template = _.template("<strong><%=nombre%></strong> - <%= cargo %>")
+    var year_ = d[0].toString().substr(0,4)
+	year = byYear.filter(function (y){return +y.key==year_;});
+	year = year.pop();
+	mujeres = year.values.filter(function(m){return m.mujer});
+	text = "<span class='right'>"+year_+"</span><strong> Legislatura:</strong> "+d[4]+"<br>";
+	text +="<strong> Mujeres:</strong> "+d[1]+"%<p>";
+	var cache = [];
+	mujeres.forEach(function(m){
+		hash = m.cargo + m.nombre; 
+		if(cache.indexOf(hash)==-1){
 			text+=template(m)+"<br>";
-		})
-		text+="</p>"
-		d3.select('.tooltip').html(text)
+			cache.push(hash);
+		}
+	})
+	text+="</p>"
+	d3.select('.tooltip').html(text)
   }
 });
